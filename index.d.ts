f931704a9d0c6e86d2182219da27ff9994a0d01d
n0f3 <n0f3@outlook.com>
2020-06-11 00:46:24 -0400

    Added missing type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "rn-apple-healthkit" {
+declare module 'rn-apple-healthkit' {
   export interface HealthKitPermissions {
     permissions: {
       read: string[];
@@ -11,7 +11,61 @@ declare module "rn-apple-healthkit" {
     limit?: number;
   }
 
+  export interface SampleOptions {
+    startDate: string;
+    endDate?: string;
+    ascending?: boolean;
+    limit?: number;
+  }
+
+  export interface StartEndDateOptions {
+    startDate: string;
+    endDate?: string;
+  }
+
+  export interface DateUnitQueryOptions {
+    unit?: string;
+    startDate: string;
+    endDate?: string;
+    ascending?: boolean;
+    limit?: number;
+  }
+
+  export interface DistanceOptions {
+    unit?: string;
+    date?: string;
+  }
+
+  export interface SampleQuery {
+    startDate: string;
+    endDate: string;
+    type: SampleQueryOptionType;
+  }
+
+  export interface UnitOnlyOptions {
+    unit: string;
+  }
+
+  export interface Date {
+    startDate: string;
+    endDate: string;
+  }
+
+  export interface Value {
+    value: number;
+  }
+
+  export interface ValueDate {
+    value: number;
+    startDate: string;
+    endDate: string;
+  }
+
   export interface AppleHealthKit {
+    authorizationStatusForType(type: string): Promise<string>;
+
+    isAvailable(callback: (error: Object, results: boolean) => void): void;
+
     initHealthKit(
       permissions: HealthKitPermissions,
       callback: (error: string, result: Object) => void
@@ -22,9 +76,34 @@ declare module "rn-apple-healthkit" {
       callback: (error: string, result: Object) => void
     ): void;
 
+    getActiveEnergyBurned(
+      options: StartEndDateOptions,
+      callback: (error: Object, resutls: Object) => void
+    ): void;
+
+    getBasalEnergyBurned(
+      options: StartEndDateOptions,
+      callback: (error: Object, results: Object) => void
+    ): void;
+
     getBiologicalSex(
       options: Object,
       callback: (error: string, result: Object) => void
+    ): void;
+
+    getBloodGlucoseSamples(
+      options: DateUnitQueryOptions,
+      callback: (error: Object, results: Array<Object>) => void
+    ): void;
+
+    getBloodPressureSamples(
+      options: DateUnitQueryOptions,
+      callback: (error: Object, results: Array<Object>) => void
+    ): void;
+
+    getBodyTemperatureSamples(
+      options: DateUnitQueryOptions,
+      callback: (error: Object, results: Array<Object>) => void
     ): void;
 
     getDateOfBirth(
@@ -37,32 +116,65 @@ declare module "rn-apple-healthkit" {
       callback: (error: string, result: Object) => void
     ): void;
 
-    saveWeight(
-      options: Object,
-      callback: (error: string, result: Object) => void
-    ): void;
-
     getHeightSamples(
-      options: Object,
-      callback: (error: string, result: Object) => void
+      options: DateUnitQueryOptions,
+      callback: (error: Object, results: Array<ValueDate>) => void
     ): void;
 
     getLatestBmi(
-      options: Object,
-      callback: (error: string, result: Object) => void
+      options: null,
+      callback: (error: string, result: ValueDate) => void
+    ): void;
+
+    getLatestBodyFatPercentage(
+      options: null,
+      callback: (error: Object, results: ValueDate) => void
+    ): void;
+
+    getBodyFatPercentageSamples(
+      options: SampleOptions,
+      callback: (error: Object, results: Array<ValueDate>) => void
+    ): void;
+
+    getLeanBodyMassSamples(
+      options: DateUnitQueryOptions,
+      callback: (err: Object, results: Array<ValueDate>) => void
     ): void;
 
     saveBmi(
-      options: Object,
+      options: Value,
       callback: (error: string, result: Object) => void
     ): void;
 
-    saveFood(
-      options: Object,
+    saveBodyFatPercentage(
+      options: Value,
+      callback: (error: Object, result: Object) => void
+    ): void;
+
+    saveLeanBodyMass(
+      options: Value,
+      callbacK: (error: Object, result: Object) => void
+    ): void;
+
+    saveWeight(
+      options: Value,
       callback: (error: string, result: Object) => void
     ): void;
 
-    isAvailable(callback: (error: Object, results: boolean) => void): void;
+    saveHeight(
+      options: Value,
+      callback: (error: string, result: Object) => void
+    ): void;
+
+    saveMindfulSession(
+      options: Date,
+      callback: (error: Object, result: Object) => void
+    ): void;
+
+    saveSteps(
+      options: ValueDate,
+      callback: (error: Object, result: Object) => void
+    ): void;
 
     getDateOfBirth(
       options: any,
@@ -70,23 +182,83 @@ declare module "rn-apple-healthkit" {
     ): void;
 
     getLatestHeight(
-      options: HealthUnitOptions,
-      callback: (err: string, results: HealthValue) => void
+      options: null,
+      callback: (err: string, results: ValueDate) => void
     ): void;
 
     getLatestWeight(
-      options: HealthUnitOptions,
-      callback: (err: string, results: HealthValue) => void
+      options: UnitOnlyOptions,
+      callback: (err: string, results: ValueDate) => void
+    ): void;
+
+    getLatestLeanBodyMass(
+      options: null,
+      callback: (err: Object, results: ValueDate) => void
     ): void;
 
     getMindfulSession(
       options: MindfulSessionData,
-      callback: (err: string, results: HealthValue) => void
+      callback: (err: string, results: Date) => void
     ): void;
 
     getStepCount(
-      options: any,
-      callback: (err: string, results: HealthValue) => void
+      options?: Object | undefined,
+      callback: (err: Object, results: Object) => void
+    ): void;
+
+    getSleepSamples(
+      options: Object,
+      callback: (err: Object, results: Array<Object>) => void
+    ): void;
+
+    getSamples(
+      options: SampleQuery,
+      callback: (err: Object, results: Array<Object>) => void
+    ): void;
+
+    getDailyDistanceWalkingRunningSamples(
+      options: SampleOptions,
+      callback: (error: string, results: Array<Object>) => void
+    ): void;
+
+    getDailyDistanceCyclingSamples(
+      options: SampleOptions,
+      callback: (error: string, results: Array<Object>) => void
+    ): void;
+
+    getDailyFlightsClimbedSamples(
+      options: SampleOptions,
+      callback: (error: string, results: Array<Object>) => void
+    ): void;
+
+    getStepCountSamples(
+      options: StartEndDateOptions,
+      callback: (error: string, results: Array<Object>) => void
+    ): void;
+
+    getDistanceCycling(
+      options: DistanceOptions,
+      callback: (error: string, results: ValueDate) => void
+    ): void;
+
+    getDistanceWalkingRunning(
+      options: DistanceOptions,
+      callback: (error: string, results: ValueDate) => void
+    ): void;
+
+    getFlightsClimbed(
+      options: Object,
+      callback: (error: Object, results: ValueDate) => void
+    ): void;
+
+    getHeartRateSamples(
+      options: DateUnitQueryOptions,
+      callback: (error: Object, results: Array<ValueDate>) => void
+    ): void;
+
+    getRespiratoryRateSamples(
+      options: DateUnitQueryOptions,
+      callback: (error: Object, results: Array<Object>) => void
     ): void;
 
     Constants: {
@@ -110,95 +282,103 @@ declare module "rn-apple-healthkit" {
     unit: HealthUnit;
   }
 
+  export enum SampleQueryOptionType {
+    Walking = 'Walking',
+    StairClimbing = 'StairClimbing',
+    Running = 'Running',
+    Cycling = 'Cycling',
+    Workout = 'Workout',
+  }
+
   export enum Permissions {
-    ActiveEnergyBurned = "ActiveEnergyBurned",
-    AppleExerciseTime = "AppleExerciseTime",
-    BasalEnergyBurned = "BasalEnergyBurned",
-    BiologicalSex = "BiologicalSex",
-    BloodGlucose = "BloodGlucose",
-    BloodPressureDiastolic = "BloodPressureDiastolic",
-    BloodPressureSystolic = "BloodPressureSystolic",
-    BodyFatPercentage = "BodyFatPercentage",
-    BodyMass = "BodyMass",
-    BodyMassIndex = "BodyMassIndex",
-    BodyTemperature = "BodyTemperature",
-    DateOfBirth = "DateOfBirth",
-    Biotin = "Biotin",
-    Caffeine = "Caffeine",
-    Calcium = "Calcium",
-    Carbohydrates = "Carbohydrates",
-    Chloride = "Chloride",
-    Cholesterol = "Cholesterol",
-    Copper = "Copper",
-    EnergyConsumed = "EnergyConsumed",
-    FatMonounsaturated = "FatMonounsaturated",
-    FatPolyunsaturated = "FatPolyunsaturated",
-    FatSaturated = "FatSaturated",
-    FatTotal = "FatTotal",
-    Fiber = "Fiber",
-    Folate = "Folate",
-    Iodine = "Iodine",
-    Iron = "Iron",
-    Magnesium = "Magnesium",
-    Manganese = "Manganese",
-    Molybdenum = "Molybdenum",
-    Niacin = "Niacin",
-    PantothenicAcid = "PantothenicAcid",
-    Phosphorus = "Phosphorus",
-    Potassium = "Potassium",
-    Protein = "Protein",
-    Riboflavin = "Riboflavin",
-    Selenium = "Selenium",
-    Sodium = "Sodium",
-    Sugar = "Sugar",
-    Thiamin = "Thiamin",
-    VitaminA = "VitaminA",
-    VitaminB12 = "VitaminB12",
-    VitaminB6 = "VitaminB6",
-    VitaminC = "VitaminC",
-    VitaminD = "VitaminD",
-    VitaminE = "VitaminE",
-    VitaminK = "VitaminK",
-    Zinc = "Zinc",
-    Water = "Water",
-    DistanceCycling = "DistanceCycling",
-    DistanceSwimming = "DistanceSwimming",
-    DistanceWalkingRunning = "DistanceWalkingRunning",
-    FlightsClimbed = "FlightsClimbed",
-    HeartRate = "HeartRate",
-    Height = "Height",
-    LeanBodyMass = "LeanBodyMass",
-    MindfulSession = "MindfulSession",
-    NikeFuel = "NikeFuel",
-    RespiratoryRate = "RespiratoryRate",
-    SleepAnalysis = "SleepAnalysis",
-    StepCount = "StepCount",
-    Steps = "Steps",
-    Weight = "Weight",
-    Workout = "Workout"
+    ActiveEnergyBurned = 'ActiveEnergyBurned',
+    AppleExerciseTime = 'AppleExerciseTime',
+    BasalEnergyBurned = 'BasalEnergyBurned',
+    BiologicalSex = 'BiologicalSex',
+    BloodGlucose = 'BloodGlucose',
+    BloodPressureDiastolic = 'BloodPressureDiastolic',
+    BloodPressureSystolic = 'BloodPressureSystolic',
+    BodyFatPercentage = 'BodyFatPercentage',
+    BodyMass = 'BodyMass',
+    BodyMassIndex = 'BodyMassIndex',
+    BodyTemperature = 'BodyTemperature',
+    DateOfBirth = 'DateOfBirth',
+    Biotin = 'Biotin',
+    Caffeine = 'Caffeine',
+    Calcium = 'Calcium',
+    Carbohydrates = 'Carbohydrates',
+    Chloride = 'Chloride',
+    Cholesterol = 'Cholesterol',
+    Copper = 'Copper',
+    EnergyConsumed = 'EnergyConsumed',
+    FatMonounsaturated = 'FatMonounsaturated',
+    FatPolyunsaturated = 'FatPolyunsaturated',
+    FatSaturated = 'FatSaturated',
+    FatTotal = 'FatTotal',
+    Fiber = 'Fiber',
+    Folate = 'Folate',
+    Iodine = 'Iodine',
+    Iron = 'Iron',
+    Magnesium = 'Magnesium',
+    Manganese = 'Manganese',
+    Molybdenum = 'Molybdenum',
+    Niacin = 'Niacin',
+    PantothenicAcid = 'PantothenicAcid',
+    Phosphorus = 'Phosphorus',
+    Potassium = 'Potassium',
+    Protein = 'Protein',
+    Riboflavin = 'Riboflavin',
+    Selenium = 'Selenium',
+    Sodium = 'Sodium',
+    Sugar = 'Sugar',
+    Thiamin = 'Thiamin',
+    VitaminA = 'VitaminA',
+    VitaminB12 = 'VitaminB12',
+    VitaminB6 = 'VitaminB6',
+    VitaminC = 'VitaminC',
+    VitaminD = 'VitaminD',
+    VitaminE = 'VitaminE',
+    VitaminK = 'VitaminK',
+    Zinc = 'Zinc',
+    Water = 'Water',
+    DistanceCycling = 'DistanceCycling',
+    DistanceSwimming = 'DistanceSwimming',
+    DistanceWalkingRunning = 'DistanceWalkingRunning',
+    FlightsClimbed = 'FlightsClimbed',
+    HeartRate = 'HeartRate',
+    Height = 'Height',
+    LeanBodyMass = 'LeanBodyMass',
+    MindfulSession = 'MindfulSession',
+    NikeFuel = 'NikeFuel',
+    RespiratoryRate = 'RespiratoryRate',
+    SleepAnalysis = 'SleepAnalysis',
+    StepCount = 'StepCount',
+    Steps = 'Steps',
+    Weight = 'Weight',
+    Workout = 'Workout',
   }
 
   export enum HealthUnit {
-    bpm = "bpm",
-    calorie = "calorie",
-    celsius = "celsius",
-    count = "count",
-    day = "day",
-    fahrenheit = "fahrenheit",
-    foot = "foot",
-    gram = "gram",
-    hour = "hour",
-    inch = "inch",
-    joule = "joule",
-    meter = "meter",
-    mgPerdL = "mgPerdL",
-    mile = "mile",
-    minute = "minute",
-    mmhg = "mmhg",
-    mmolPerL = "mmolPerL",
-    percent = "percent",
-    pound = "pound",
-    second = "second"
+    bpm = 'bpm',
+    calorie = 'calorie',
+    celsius = 'celsius',
+    count = 'count',
+    day = 'day',
+    fahrenheit = 'fahrenheit',
+    foot = 'foot',
+    gram = 'gram',
+    hour = 'hour',
+    inch = 'inch',
+    joule = 'joule',
+    meter = 'meter',
+    mgPerdL = 'mgPerdL',
+    mile = 'mile',
+    minute = 'minute',
+    mmhg = 'mmhg',
+    mmolPerL = 'mmolPerL',
+    percent = 'percent',
+    pound = 'pound',
+    second = 'second',
   }
 
   const appleHealthKit: AppleHealthKit;
